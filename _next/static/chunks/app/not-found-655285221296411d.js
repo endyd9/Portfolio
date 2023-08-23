42273a3dd0883c49292eab726865a26b6a2160a8
endyd9 <endyd9@gmail.com>
2023-08-23 17:15:45 +0900

    Updates

--- _next/static/chunks/app/not-found-655285221296411d.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{7982:function(e,r,t){Promise.resolve().then(t.bind(t,3335))},3335:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return o}});var n=t(7437);function o(){return(0,n.jsx)("main",{className:"w-full h-full",children:(0,n.jsx)("p",{className:"mt-[70%] text-2xl text-center",children:"없는 페이지 입니다"})})}},622:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function l(e,r,t){var n,s={},l=null,i=null;for(n in void 0!==t&&(l=""+t),void 0!==r.key&&(l=""+r.key),void 0!==r.ref&&(i=r.ref),r)u.call(r,n)&&!c.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:l,ref:i,props:s,_owner:f.current}}r.Fragment=s,r.jsx=l,r.jsxs=l},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=7982)}),_N_E=e.O()}]);
