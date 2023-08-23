af00eff5eb11ade281464a62eda4079309eb4122
endyd9 <endyd9@gmail.com>
2023-08-23 17:44:12 +0900

    넥스트 컨피그 재수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+const repository = "joonyoung-s-portfolio-page";
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
