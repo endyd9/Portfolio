66e5e5a56bd3a765f561db83c7b8c9ad4ccaa1ec
endyd9 <75965657+endyd9@users.noreply.github.com>
2023-08-24 14:54:25 +0000

    Deploying to gh-pages from @ endyd9/Portfolio@7af789a77150c39d7941932fee238d93a5b23881 🚀

--- _next/static/chunks/app/page-ce6b505f3ca66242.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4076:function(e,t,r){Promise.resolve().then(r.bind(r,5003))},5003:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return o}});var n=r(7437),s=r(2265);function o(){let[e,t]=(0,s.useState)(""),r=async()=>{let e=0,r="성장중인 개발자 김두용입니다.";for(t("");e!==r.length;)t(t=>t+r[e]),await new Promise(e=>setTimeout(e,150)),e+=1};return(0,s.useEffect)(()=>{setTimeout(()=>r(),1500)},[]),(0,n.jsx)("main",{id:"home",className:"w-screen h-screen animate-[intro_2s] bg-small lg:bg-big bg-cover",children:(0,n.jsx)("div",{className:"w-full h-[90%] flex items-center justify-center",children:(0,n.jsx)("h2",{className:"text-3xl text-white animate-cursor",children:e})})})}},622:function(e,t,r){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=r(2265),s=Symbol.for("react.element"),o=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,c=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function a(e,t,r){var n,o={},a=null,f=null;for(n in void 0!==r&&(a=""+r),void 0!==t.key&&(a=""+t.key),void 0!==t.ref&&(f=t.ref),t)i.call(t,n)&&!u.hasOwnProperty(n)&&(o[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===o[n]&&(o[n]=t[n]);return{$$typeof:s,type:e,key:a,ref:f,props:o,_owner:c.current}}t.Fragment=o,t.jsx=a,t.jsxs=a},7437:function(e,t,r){"use strict";e.exports=r(622)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=4076)}),_N_E=e.O()}]);
