37dae828931d3afc14a96fc6caf4d2a5daee250c
endyd9 <endyd9@gmail.com>
2023-08-25 00:12:43 +0900

    pc레이아웃 완료

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
   }, [showMenu]);
   return (
     <header className="w-full h-[10%] fixed left-0 top-0 z-10">
-      <div className="h-full flex items-center justify-between relative bg-black px-4 z-10">
+      <div className="h-full flex items-center justify-between relative bg-black px-4 z-10 xl:z-0">
         <Link href={`${prefix}/`}>
           <h1 className="text-white text-4xl font-extralight">
             DY's PortFolio
@@ -60,7 +60,7 @@ export default function Header() {
       </div>
       <div
         id="menu_content"
-        className="w-full h-[10%] fixed left-0 bg-[rgba(0,0,0,0.5)] top-0"
+        className="w-full h-[10%] fixed bg-[rgba(0,0,0,0.5)] top-0 xl:bg-inherit xl:w-[40%] xl:left-none xl:right-4 2xl:top-2"
       >
         <nav className="h-20 text-white text-2xl z-10">
           <ul className="h-full flex justify-between items-center mx-4">
